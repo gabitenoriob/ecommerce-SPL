472cf62b46543317dce474fc3701472c387525fb
gabitenoriob <gabitenoriob@gmail.com>
2025-10-28 10:32:29 -0300

    update catalogo

--- backend/ms-catalogo/config.py
+++ backend/ms-catalogo/config.py
@@ -6,5 +6,6 @@ class Settings(BaseSettings):
     # que foi injetada pelo docker-compose.yml
     database_url: str = os.environ.get("DATABASE_URL")
 
+
 # Instância única que será usada em todo o app
 settings = Settings()
